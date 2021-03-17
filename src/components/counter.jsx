29143f74c813f06438c5ac2a13b61f78cff01964
melkam <melkammekonnen899@gmail.com>
2021-03-17 09:08:02 +0300

    Add a state and a style for counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        imageUrl: "https://picsum.photos/200"
     };
 
+    styles = {
+        fontSize: 10,
+        fontWeight: "bold"
+    }
+
     formatCount(){
-        const { count } = this.state.count;
+        const { count } = this.state;
         return count === 0 ? "Zero" : count;
     }
 
     render() { 
         return (
             <div>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <img src={this.state.imageUrl} alt="" />
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
