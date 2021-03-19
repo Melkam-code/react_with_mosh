9c2558983dd97a37217f499eb7b578779a2a6099
melkam <melkammekonnen899@gmail.com>
2021-03-19 10:16:59 +0300

    Building constructor and define an incrementing function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,6 +6,11 @@ class Counter extends Component {
         tags: ['tag1','tag2','tag3']
     };
 
+    constructor(){
+        super();
+        this.handleincrement = this.handleincrement.bind(this);
+    }
+
     styles = {
         fontSize: 10,
         fontWeight: "bold"
@@ -20,11 +25,17 @@ class Counter extends Component {
         let classes = "badge m-2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary";
         return classes;
-    }
+    } 
 
     renderTags(){
         if(this.state.tags.length === 0) return <p>There are no tags</p>;
         return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    }
+
+    handleincrement = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
     }
 
     render() { 
@@ -32,7 +43,9 @@ class Counter extends Component {
         return (
             <div>
                 <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+
+                <button onClick={this.handleincrement} className="btn btn-secondary btn-sm">Increment</button>
+
                 {this.state.tags.length === 0 && "Please create a new tag"}
                 { this.renderTags() }
             </div>
